556e7d4e8b38ad8be1e6d7b90beec7ffff9c151a
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-22 20:58:55 +0900

    ear
    at: ZM -task
    
    feat: "ZL -task
    
    qw
    feat: ZL -task
    "

--- train.ts
+++ train.ts
@@ -21,3 +21,20 @@ function stringToKebab(input: string): any {
 }
 
 console.log(stringToKebab('I Love Kebab'));
+/**
+
+ZM-TASK:
+
+Shunday function yozing, u function parametrga berilgan raqamlarni orqasiga ogirib qaytarsin.
+MASALAN: reverseInteger(123456789) return 987654321
+
+
+
+**/
+
+function reverseInteger(input: number): any {
+	const toString: string = '' + input;
+	return Number(toString.split('').reverse().join(''));
+}
+
+console.log(reverseInteger(123456789));
