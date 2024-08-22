dd5638942d18f1cd22519cde81a947d1aac5e9cb
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-22 21:11:31 +0900

    feat: ZM -task

--- train.ts
+++ train.ts
@@ -1,28 +1,5 @@
 /**
 
-ZL-TASK:
-
-Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
-MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
-
-
-**/
-
-function stringToKebab(input: string): any {
-	const str = input.split(' ');
-	let vacant: any[] = [];
-	for (let word of str) {
-		vacant.push(word.toLowerCase());
-	}
-
-	// console.log(vacant);
-
-	return vacant.join('-');
-}
-
-console.log(stringToKebab('I Love Kebab'));
-/**
-
 ZM-TASK:
 
 Shunday function yozing, u function parametrga berilgan raqamlarni orqasiga ogirib qaytarsin.
