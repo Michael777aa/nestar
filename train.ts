1e9d9b6815491467fd5c5c6defc5edb6652c14c8
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-09-08 13:37:44 +0900

    feat: task -ZS

--- train.ts
+++ train.ts
@@ -1,23 +1,19 @@
 /**
-ZR-TASK:
+ZS-TASK:
 
-Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin.
-MASALAN: countNumberAndLetters(“string152%\¥”) return {number:3, letter:6}
+Shunday function yozing, u parametridagi arrayni ichidagi 1 marta kelgan elemnetni qaytarsin.
+MASALAN: singleNumber([4, 2, 1, 2, 1]) return 4
 **/
 
-function countNumberAndLetters(input: string): { number: number; letter: number } {
-	let numberCount = 0;
-	let letterCount = 0;
-
-	for (let char of input) {
-		if (/[0-9]/.test(char)) {
-			numberCount++;
-		} else if (/[a-zA-Z]/.test(char)) {
-			letterCount++;
+function singleNumber(nums: number[]): number {
+	const freq = new Array(101).fill(0);
+	for (const num of nums) {
+		freq[num]++;
+	}
+	for (let i = 0; i < freq.length; i++) {
+		if (freq[i] === 1) {
+			return i;
 		}
 	}
-
-	return { number: numberCount, letter: letterCount };
 }
-
-console.log(countNumberAndLetters('string152%¥'));
+console.log(singleNumber([4, 2, 1, 2, 1]));
