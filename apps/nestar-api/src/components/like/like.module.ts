4beffefc549331552ca229bc1db16384b72e447e
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-09-08 12:22:20 +0900

    feat: create likeService model

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import LikeSchema from '../../schemas/Like.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { LikeService } from './like.service';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Like', schema: LikeSchema }]), AuthModule, ViewModule],
+	providers: [LikeService],
+	exports: [LikeService],
+})
 export class LikeModule {}
