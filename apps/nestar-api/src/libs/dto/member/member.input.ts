81f9b630135ed2b1bbdf792056550f6ecd579266
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-10-15 20:23:49 +0900

    fix: modify member input

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -100,7 +100,7 @@ export class MembersInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn([availableMemberSorts])
+	@IsIn(availableMemberSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
